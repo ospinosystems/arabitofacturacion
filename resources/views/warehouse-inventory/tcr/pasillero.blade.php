bab3132e8585374596d2e3e2109c118620bd859e
Alvaro Ospino <34920387+alvaritojose2712@users.noreply.github.com>
2025-11-22 13:59:19 -0400

    fd

--- resources/views/warehouse-inventory/tcr/pasillero.blade.php
+++ resources/views/warehouse-inventory/tcr/pasillero.blade.php
@@ -1869,11 +1869,9 @@ function mostrarReporteNovedades(novedades) {
             </div>
         ` : '';
         
-        /* const observacionesHtml = n.observaciones ? `
-            <div class="mt-1 text-[10px] text-gray-600 italic">
-                <i class="fas fa-comment mr-1"></i>${n.observaciones}
-            </div>
-        ` : ''; */
+        const observacionesHtml = n.observaciones ? `
+            
+        ` : '';
         
         return `
             <div class="border border-gray-200 rounded-lg p-2 mb-2">
